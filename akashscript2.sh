2214112fbed696344787fd846f6f34a84bbc3817
akashm77 <akashm77@gmail.com>
2024-12-21 10:13:17 +0530

    Added a feature in akashscript2.sh

--- akashscript2.sh
+++ akashscript2.sh
@@ -5,3 +5,4 @@ echo "This is a new file created by akashscript2.sh" > akash2output.txt
 echo "The script executed successfully!" >> akash2output.txt
 cat akash2output.txt
 
+echo 'Rebased feature added'
