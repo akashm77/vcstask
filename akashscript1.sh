8fc3738a95ca5f4068255d02ab792745c1abefe2
akashm77 <akashm77@gmail.com>
2024-12-21 09:27:45 +0530

    Initial commit with Python and Shell scripts

---
+++ akashscript1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Simple Hello Guvi script
+echo "Hello, Guvi,welcome to India!"
+
---
+++ akashscript2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create a new file and add content
+echo "This is a new file created by akashscript2.sh" > akash2output.txt
+echo "The script executed successfully!" >> akash2output.txt
+cat akash2output.txt
+
